769955c609f8ea2c1e86f361be7d133c905f0b9c
SonyaKorni <sone4ko2000@gmail.com>
2020-06-12 09:58:20 +0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WidgetLeftComponent } from './widget-left/widget-left.component';
+import { WidgetRightComponent } from './widget-right/widget-right.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WidgetLeftComponent
+    WidgetLeftComponent,
+    WidgetRightComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
